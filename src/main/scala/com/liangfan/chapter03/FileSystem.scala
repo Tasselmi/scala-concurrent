7a56e21cbb01ac16e58d36f719730667650a3620
liangfan <tasselmi@yeah.net>
2019-07-16 13:58:22 +0800

    finished writing a file-system

--- src/main/scala/com/liangfan/chapter03/FileSystem.scala
+++ src/main/scala/com/liangfan/chapter03/FileSystem.scala
@@ -2,6 +2,7 @@ package com
 package liangfan
 package chapter03
 
+
 import java.util.concurrent._
 import java.util.concurrent.atomic._
 
@@ -10,16 +11,36 @@ import java.io.File
 
 import org.apache.commons.io.FileUtils
 
+import scala.annotation.tailrec
 import scala.collection.concurrent
+
 
 object FileSystemTest extends App {
     val fileSystem = new FileSystem(".")
+
     fileSystem.logMessage("testing log!")
-    fileSystem.deleteFile("test.txt")
+    fileSystem.copyFile("build.sbt", "text.txt")
+    fileSystem.deleteFile("text.txt")
+    fileSystem.deleteFile("build.sbt.backup")
+
+    val rootFiles = fileSystem.filesInDir("") //注意这方法是会造成阻塞的，如果先运行这个，会和deleteFile冲突
+    log("All files in the root dir: " + rootFiles.mkString(", "))
 }
 
 
 class FileSystem(val root: String) {
+
+    private val messages = new LinkedBlockingQueue[String]()
+
+    val logger = new Thread {
+        setDaemon(true)
+        override def run(): Unit = while (true) log(messages.take())
+    }
+    logger.start()
+
+    def logMessage(msg: String): Unit = messages.add(msg)
+
+
     sealed trait State
     class Idle extends State
     class Creating extends State
@@ -30,15 +51,15 @@ class FileSystem(val root: String) {
         val state = new AtomicReference[State](new Idle)
     }
 
-    val rootDir = new File(root)
-    val files: concurrent.Map[String, Entry] =
+    val files: concurrent.Map[String, Entry] = new concurrent.TrieMap()
         //new ConcurrentHashMap().asScala
-        new concurrent.TrieMap()
-    for (f <- FileUtils.iterateFiles(rootDir, null, false).asScala) {
+
+    for (f <- FileUtils.iterateFiles(new File(root), null, false).asScala) {
         files.put(f.getName, new Entry(false))
     }
 
-    def prepareForDelete(entry: Entry): Boolean = {
+    @tailrec
+    private def prepareForDelete(entry: Entry): Boolean = {
         val s0 = entry.state.get()
         s0 match {
             case i: Idle => {  //空闲的文件才能被删除，如果空闲，那么试着改变其状态为Deleting
@@ -61,22 +82,70 @@ class FileSystem(val root: String) {
         files.get(fileName) match {
             case None => logMessage(s"path '$fileName' does not exist!")
             case Some(entry) if entry.isDir => logMessage(s"path '$fileName' is a directory!")
-            case Some(entry) => execute {
-                if (prepareForDelete(entry)) {
-                    if (FileUtils.deleteQuietly(new File(fileName))) files.remove(fileName)
+            case Some(ent) => execute {
+                if (prepareForDelete(ent)) {
+                    if (FileUtils.deleteQuietly(new File(fileName))) {
+                        files.remove(fileName)
+                        println(s"File $fileName has been deleted !")
+                    }
                 }
             }
         }
     }
 
-
-    private val messages = new LinkedBlockingQueue[String]()
-
-    val logger: Thread = new Thread {
-        setDaemon(true)
-        override def run(): Unit = while (true) log(messages.take())
+    @tailrec
+    private def acquire(entry: Entry): Boolean = {
+        val s0 = entry.state.get()
+        s0 match {
+            case _: Creating | _: Deleting => {
+                logMessage("file inaccessible, cannot copy.")
+                false
+            }
+            case i: Idle => { //如果是空闲状态，那么将其变为Copying状态
+                if (entry.state.compareAndSet(s0, new Copying(1))) true
+                else acquire(entry)
+            }
+            case c: Copying => { //如果已经是Copying状态，那么n代表其第n次copy，将子状态n加1
+                if (entry.state.compareAndSet(s0, new Copying(c.n + 1))) true
+                else acquire(entry)
+            }
+        }
     }
-    logger.start()
 
-    def logMessage(msg: String): Unit = messages.add(msg)
+    @tailrec
+    private def release(entry: Entry): Unit = {
+        val s0 = entry.state.get()
+        s0 match {
+            case c: Creating => { if (!entry.state.compareAndSet(s0, new Idle)) release(entry) }
+            case cp: Copying => {
+                val nstate = if (cp.n == 1) new Idle else new Copying(cp.n - 1)
+                if (!entry.state.compareAndSet(s0, nstate)) release(entry)
+            }
+            case i: Idle => sys.error("Error - released more times than acquired.")
+            case d: Deleting => sys.error("Error - releasing a file that is being deleted!")
+        }
+    }
+
+    def copyFile(src: String, dest: String): Unit = {
+        files.get(src) match {
+            case Some(srcEntry) if !srcEntry.isDir => execute {
+                if (acquire(srcEntry)) try {
+                    val destEntry = new Entry(isDir = false)
+                    destEntry.state.set(new Creating)
+                    if (files.putIfAbsent(dest, destEntry).isEmpty) try {
+                        FileUtils.copyFile(new File(src), new File(dest))
+                    } finally release(destEntry)
+                } finally release(srcEntry)
+            }
+        }
+    }
+
+//    def filesInDir(dir: String): Iterable[String] = {
+//        for ((name, state) <- files; if name.startsWith(dir)) yield name
+//    }
+
+    def filesInDir(dir: String): Iterable[String] = {
+        // trie map snapshots
+        for ((name, state) <- files; if name.startsWith(dir)) yield name
+    }
 }
